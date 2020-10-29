c88f1b073e3f67775ca4e979b0d7984c27dcaf8b
Douglas Silva <dogusky.silva@outlook.com>
2020-10-28 22:02:06 -0300

    limpa o arquivo main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobileapp/screens/products/products_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -32,9 +33,13 @@ class _MyHomePageState extends State<MyHomePage> {
   static TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static List<Widget> _widgetOptions = <Widget>[
-    HomePage(),
+    ProductsScreen(),
     Text(
       'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
       style: optionStyle,
     ),
     Text(
@@ -49,68 +54,31 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  void _incrementCounter() {
-    Navigator.push(context, MaterialPageRoute(builder: (context) => Details()));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BottomNavigationBar Sample'),
-      ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+            icon: Icon(Icons.store),
+            label: 'Produtos',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.business),
-            label: 'Business',
+            label: 'Marketing',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
+            icon: Icon(Icons.shopping_cart),
+            label: 'Pedidos',
           ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile')
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
         onTap: _onItemTapped,
-      ),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => Details()));
-        },
-        child: Text('Go to'),
-      ),
-    );
-  }
-}
-
-class Details extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Return to List'),
-        ),
       ),
     );
   }
