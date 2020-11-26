99f07341fc4fc3e75a31c83974c272a73812664b
Douglas Silva <dogusky.silva@outlook.com>
2020-11-26 19:35:45 -0300

    adiciona share para o produto

--- lib/screens/order/order_screen.dart
+++ lib/screens/order/order_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:typed_data';
+
+import 'package:esys_flutter_share/esys_flutter_share.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mobileapp/models/order.dart';
+import 'package:mobileapp/services/bitly_service.dart';
 
 class OrderScreen extends StatefulWidget {
   final Order order;
@@ -32,12 +37,39 @@ class _OrderScreenState extends State<OrderScreen> {
         state: StepState.disabled),
   ];
 
+  _share() {
+    BitLyRequests()
+        .fetchShortLink(
+            "https://storm-tech.herokuapp.com/products?reseller=XFTH3478")
+        .then((value) async {
+      String shareText =
+          "Oie, Nat aqui de novo! Queria sabe contigo se gostou da experiência. Fala pra mim de 0 a 10 qual as chances de você recomendar a gente pra algum amigo ou alguém da família?";
+
+      ByteData bytes = await rootBundle.load('assets/nati_share.jpeg');
+
+      await Share.file('Aline Pilot', 'nati_share.jpeg',
+          bytes.buffer.asUint8List(), 'image/jpeg',
+          text: shareText);
+
+      print(shareText);
+    }).catchError((error) {
+      print(error);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         key: _scaffoldKey,
         appBar: AppBar(
-          title: Text('Ola Mundo'),
+          title: Text("Pedido #${widget.order.id.toString()}"),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.share),
+              onPressed: _share,
+              color: Colors.white,
+            )
+          ],
         ),
         body: Column(children: <Widget>[
           Expanded(
